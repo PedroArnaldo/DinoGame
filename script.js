0a28b56b7e90305ea0f726ea03ae66acb517b5e4
PedroArnaldo <pedromadeira201502@outlook.com>
2020-10-16 12:19:12 -0300

    add cactus

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
 let isJumping = false;
+
 function handleKeyUp(event)
 {
     if(event.keyCode === 32)
@@ -27,10 +29,12 @@ function Jump()
             //Descendo
             let downInterval = setInterval(() =>{
                 if (position <= 0){
+
                     clearInterval(downInterval);
                     isJumping = false;
                 }
                 else{
+
                     position -=20;
                     dino.style.bottom = position + 'px';
                 }
@@ -50,4 +54,17 @@ function Jump()
     }, 20); //vai ser executado a cada 20 ms
 }
 
+function Cactus() {
+
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+
+}
+
+Cactus();
 document.addEventListener('keyup', handleKeyUp);
